2e299b829bc342eebf172226ce93b45013c8032a
Hemanth R <hemanth.r.official@gmail.com>
2023-09-06 21:11:17 +0100

    Prime Numbers

---
+++ Prime_Number.py
@@ -0,0 +1,21 @@
+def Prime(n):
+    i=n-1
+    c=0
+    while(i>1):
+        if(n%i==0):
+            c+=1 
+        i-=1
+    if(c>0):
+        return 0
+    return 1
+
+max=10
+print("Prime number upto 10")
+if(max==1):
+    print(max)
+else:
+    print(1,end=" ")
+    for i in range(max+1):
+        if(Prime(i)):
+            print(i,end=" ")
+    
